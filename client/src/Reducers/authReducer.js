1cc18118bcd0af54f1352c185bdf5a8d47394aa4
Brian Thomas <Brianthomas571@gmail.com>
2019-02-28 16:24:13 -0600

    adding redux boilerplate with authreducer and store for state management

---
+++ client/src/Reducers/authReducer.js
@@ -0,0 +1,12 @@
+// authentication reducer
+const initalState = {
+  isAuthenticated: false,
+  user: {}
+};
+
+export default function(state = initalState, action) {
+  switch (action.type) {
+    default:
+      return state;
+  }
+}
