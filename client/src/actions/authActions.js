302cdc39ed9166ed0a5e390596aefae579c431e6
Brian Thomas <Brianthomas571@gmail.com>
2019-03-01 13:20:38 -0600

    set login action with validation like server side validation

--- client/src/actions/authActions.js
+++ client/src/actions/authActions.js
@@ -1,7 +1,10 @@
-// register user actions
 import { GET_ERRORS } from "./types";
+import { SET_CURRENT_USER } from "./types";
 import axios from "axios";
+import setAuthToken from "../uilts/setAuthToken";
+import jwt_decode from "jwt-decode";
 
+// register user actions
 export const registerUser = (userData, history) => dispatch => {
   // using axios to fetch the apis from server side routes
   axios
@@ -13,4 +16,37 @@ export const registerUser = (userData, history) => dispatch => {
         payload: err.response.data
       })
     );
+};
+
+// login get user token
+export const loginUser = userData => dispatch => {
+  axios
+    .post("/pi/users/login", userData)
+    .then(res => {
+      // save to local storage
+      // pull token out of the data
+      const { token } = res.data;
+      // set token to local storage
+      localStorage.setItem("jwtToken", token);
+      // set token to auth header
+      setAuthToken(token);
+      // Decode token to get user data
+      const decoded = jwt_decode(token);
+      // set current user
+      dispatch(setCurrentUser(decoded));
+    })
+    .catch(err =>
+      dispatch({
+        type: GET_ERRORS,
+        payload: err.response.data
+      })
+    );
+};
+
+// set logged in user
+export const setCurrentUser = decoded => {
+  return {
+    type: SET_CURRENT_USER,
+    payload: decoded
+  };
 };
