d1ba780758917c24274395c95babbc94bbf0961e
Brian Thomas <Brianthomas571@gmail.com>
2019-03-03 23:15:27 -0600

    created add experience ui component

--- client/src/App.js
+++ client/src/App.js
@@ -16,6 +16,7 @@ import { clearCurrentProfile } from "./actions/profileActions";
 import PrivateRoute from "./components/common/PrivateRoute";
 import CreateProfile from "./components/create-profile/CreateProfile";
 import EditProfile from "./components/edit-profile/EditProfile";
+import AddExperience from "./components/add-credentials/AddExperience";
 
 // check for token (to see if user is still logged in)
 if (localStorage.jwtToken) {
@@ -64,6 +65,13 @@ class App extends Component {
                   exact
                   path="/edit-profile"
                   component={EditProfile}
+                />
+              </Switch>
+              <Switch>
+                <PrivateRoute
+                  exact
+                  path="/add-experience"
+                  component={AddExperience}
                 />
               </Switch>
             </div>
