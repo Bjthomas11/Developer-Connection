40e80ca86b4098432bb377e3e31b00050dc11efb
Brian Thomas <Brianthomas571@gmail.com>
2019-03-15 11:14:21 -0500

    prepared for deploy

--- server.js
+++ server.js
@@ -2,12 +2,11 @@ const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const passport = require("passport");
-
 const users = require("./routes/api/users");
 const profile = require("./routes/api/profile");
 const posts = require("./routes/api/posts");
-
 const app = express();
+const path = require("path");
 
 // Body parser middleware
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -32,6 +31,16 @@ require("./config/passport")(passport);
 app.use("/api/users", users);
 app.use("/api/profile", profile);
 app.use("/api/posts", posts);
+
+// Server static assests if in prod
+if (process.env.NODE_ENV === "production") {
+  // set static folder
+  app.use(express.static("client/build"));
+  // for any route load react index html build file
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 
 const port = process.env.PORT || 5000;
 
