458c81a486359f7eafadb13156251d4735439351
Brian Thomas <Brianthomas571@gmail.com>
2019-03-15 11:23:52 -0500

    working on deploy

--- server.js
+++ server.js
@@ -32,11 +32,11 @@ app.use("/api/users", users);
 app.use("/api/profile", profile);
 app.use("/api/posts", posts);
 
-// Server static assests if in prod
+// Server static assets if in production
 if (process.env.NODE_ENV === "production") {
-  // set static folder
+  // Set static folder
   app.use(express.static("client/build"));
-  // for any route load react index html build file
+
   app.get("*", (req, res) => {
     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
   });
